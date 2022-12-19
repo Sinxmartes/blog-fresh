dad0ed6b0a5fc861b753b0746c699a980214b92a
Sinxmartes <marco.paulo.rm@gmail.com>
2022-12-19 14:52:59 -0300

    Fix: utils GetPosts
    Fix bug function GetPosts for not read .gitkeep file
    
    Signed-off-by: Sinxmartes<marco.paulo.rm@gmail.com>

--- utils/posts.ts
+++ utils/posts.ts
@@ -10,8 +10,10 @@ export async function getPosts(): Promise<Post[]> {
     const files = Deno.readDir("./posts");
     const promises = [];
     for await (const file of files) {
-      const slug = file.name.replace(".md", "");
-      promises.push(getPost(slug));
+      if(file.name != ".gitkeep") {
+        const slug = file.name.replace(".md", "");
+        promises.push(getPost(slug));
+      }
     }
     const posts = await Promise.all(promises) as Post[];
     posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
